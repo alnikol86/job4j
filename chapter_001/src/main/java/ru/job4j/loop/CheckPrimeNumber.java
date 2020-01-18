d5f678a96adb3f0d0b3f758ea3008cd6b7d00436
Alexander Nikolaev <nikolaevairman@gmail.com>
2020-01-18 21:44:35 +0300

    [#218894]refactoring

--- chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,11 +3,11 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
     public static boolean check(int num) {
         boolean prime = true;
-        for (int i = 2; i <= (num-1); i++) {
+        for (int i = 2; i < num; i++) {
         if ((num % i) == 0) {
             prime = false;
         break;
-        } else {continue;}
+        }
         }
         return prime;
     }
