febcf11b9c80414651af4c60a63bc666b69ff1e7
Alexander Nikolaev <nikolaevairman@gmail.com>
2020-01-22 21:18:40 +0300

    [#218834]

--- chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
+++ chapter_001/src/main/java/ru/job4j/array/ArrayChar.java
@@ -4,10 +4,8 @@ public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
         boolean result = true;
         for (int i = 0; i < pref.length; i++) {
-            for (int i1 = i; i1 <= i; i1++) {
-                if (pref[i] != word[i1]) {
+                if (pref[i] != word[i]) {
                     result = false;
-                }
             }
         }
         return result;
