2549c095ad97ee1dbdd0409097aad8eae1fa64a1
Alexander Nikolaev <nikolaevairman@gmail.com>
2020-01-24 13:37:09 +0300

    [#218841]

--- chapter_001/src/main/java/ru/job4j/array/Min.java
+++ chapter_001/src/main/java/ru/job4j/array/Min.java
@@ -13,7 +13,7 @@ public class Min {
      */
     public static int findMin(int[] array) {
         int min = array[0];
-        for (int i = 0; i <= array.length - 1; i++) {
+        for (int i = 0; i < array.length; i++) {
             if (min > array[i]) {
                 min = array[i];
             }
