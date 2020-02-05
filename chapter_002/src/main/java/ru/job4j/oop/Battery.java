52fe0bc7f6cba856d47413b595fe23f68a0b17ad
Alexander Nikolaev <nikolaevairman@gmail.com>
2020-02-05 19:49:28 +0300

    1.7. Состояние объекта.[#218986]

--- chapter_002/src/main/java/ru/job4j/oop/Battery.java
+++ chapter_002/src/main/java/ru/job4j/oop/Battery.java
@@ -3,6 +3,10 @@ package ru.job4j.oop;
 public class Battery {
     private int load;
 
+    public void battery(int charge) {
+        this.load = charge;
+    }
+
     public void exchange(Battery another) {
         this.load = this.load + another.load;
         another.load = 0;
