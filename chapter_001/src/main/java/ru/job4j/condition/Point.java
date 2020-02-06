ec8b659a4bad8a1d4e27bc5a1714dc1f057e64da
Alexander Nikolaev <nikolaevairman@gmail.com>
2020-02-06 19:01:07 +0300

    4. Расстояние между точками в трехмерном пространстве.[#218978]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -4,16 +4,35 @@ import static java.lang.Math.sqrt;
 import static java.lang.Math.pow;
 
 public class Point {
+    /**
+     * It is the fields for the object
+     */
     private int x;
     private int y;
+    private int z;
 
+    /**
+     * A constructor for a start position
+     * @param first X coordinate
+     * @param second Y coordinate
+     */
     public Point(int first, int second) {
         this.x = first;
         this.y = second;
     }
 
+    public  Point(int first, int second, int third) {
+        this.x = first;
+        this.y = second;
+        this.z = third;
+    }
+
     public double distance(Point that) {
         return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
+    }
+
+    public double distance3d(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2) + pow(this.z - that.z, 2));
     }
 
 public static void main(String[] args) {
@@ -32,6 +51,9 @@ public static void main(String[] args) {
         Point one4 = new Point(9, 5);
         Point two4 = new Point(4, 5);
         System.out.println("Distance4 = " + one4.distance(two4));
+
+        Point one5 = new Point(9, 5, 6);
+        Point two5 = new Point(4, 5, 2);
+        System.out.println("distance3D = " + one5.distance3d(two5));
     }
 }
-
--- chapter_001/src/test/java/ru/job4j/condition/PointTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -12,4 +12,13 @@ public class PointTest {
         double out = one.distance(two);
         Assert.assertEquals(expected, out, 0.01);
     }
+    @Test
+    public void distance3D() {
+        Point one = new Point(4, 3, 2);
+        Point two = new Point(1, 6, 3);
+        Point three = new Point(2, 4, 3);
+        double expected = 4.35;
+        double out = one.distance3d(two);
+        Assert.assertEquals(expected, out, 0.01);
+    }
 }
