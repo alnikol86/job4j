3b1153fc259f031fe746e62c8ca2db0f58ef13a5
Alexander Nikolaev <nikolaevairman@gmail.com>
2020-02-07 17:35:58 +0300

    2. Перегрузка конструктора.[#218992]

--- chapter_002/src/main/java/ru/job4j/oop/Animal.java
+++ chapter_002/src/main/java/ru/job4j/oop/Animal.java
@@ -1,8 +1,14 @@
 package ru.job4j.oop;
 
 public class Animal {
+    private String name;
+
     public Animal() {
         super();
         System.out.println("Load Animal");
+    }
+
+    public Animal(String name) {
+        this.name = name;
     }
 }
