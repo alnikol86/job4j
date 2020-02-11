7776d9a3920a3caf9f5d05f1beb4902d275c4072
Alexander Nikolaev <nikolaevairman@gmail.com>
2020-02-11 17:13:20 +0300

    0. Видео[#219003]

---
+++ chapter_002/src/main/java/ru/job4j/encapsulation/Config.java
@@ -0,0 +1,20 @@
+package ru.job4j.encapsulation;
+
+public class Config {
+    private String name;
+    private int position;
+    private String[] properties;
+
+    public Config(String name) {
+        this.name = name;
+    }
+    private void print() {
+        System.out.println(position);
+    }
+    public String search(String key) {
+        return key;
+    }
+    private String getProperty(String key) {
+        return search(key);
+    }
+}
