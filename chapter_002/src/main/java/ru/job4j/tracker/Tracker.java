fc898612dc803c2aa9237ae732d1b40c6933fd9a
Alexander Nikolaev <nikolaevairman@gmail.com>
2020-02-17 00:28:16 +0300

    draft 219002

--- chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
+++ chapter_002/src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,4 +1,19 @@
 package ru.job4j.tracker;
 
 public class Tracker {
+    private final Item[] items = new Item[100];
+    private int position = 0;
+
+    public Item add(Item item) {
+
+    }
+    public Item[] findAll() {
+
+    }
+    public Item[] findByName(String key) {
+
+    }
+    public Item findById(String id) {
+
+    }
 }
