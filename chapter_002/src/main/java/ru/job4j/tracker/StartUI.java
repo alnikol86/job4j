c7ff8c270552b8fedc3187475b0340c0c4ce17ea
Alexander Nikolaev <nikolaevairman@gmail.com>
2020-04-05 00:11:01 +0300

    4.2. Статические методы.[#218962]

--- chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
+++ chapter_002/src/main/java/ru/job4j/tracker/StartUI.java
@@ -3,6 +3,51 @@ package ru.job4j.tracker;
 import java.util.Scanner;
 
 public class StartUI {
+
+    public static void createItem(Input input, Tracker tracker) {
+        System.out.println("==== Create a new Item ====");
+        String name = input.ascStr("Enter name: ");
+        Item item = new Item(name);
+        tracker.add(item);
+    }
+    public static void allItems(Input input, Tracker tracker) {
+        System.out.println("==== Tracker has contained ====");
+        Item[] items = tracker.findAll();
+        for (int i = 0; i < items.length; ++i) {
+            System.out.println("Name: " + items[i].getName()
+                    + ", ID: " + items[i].getId());
+        }
+    }
+    public static void editItem(Input input, Tracker tracker) {
+        System.out.println("==== Edit item ====");
+        String id = input.ascStr("Enter the item ID for replace: ");
+        String name = input.ascStr("Enter a new item name: ");
+        Item item = new Item(name);
+        String result = tracker.replace(id, item) ? "The operation was successful" : "The operation wasn't successful. Check ID for the item.";
+        System.out.println(result);
+    }
+    public static void deleteItem(Input input, Tracker tracker) {
+        System.out.println("==== Delete item ====");
+        String id = input.ascStr("Enter Item id: ");
+        String result = tracker.delete(id) ? "The Item was deleted." : "The Item was not deleted. Check ID for the Item.";
+        System.out.println(result);
+    }
+    public static void findItemId(Input input, Tracker tracker) {
+        System.out.println("==== Find item by id ====");
+        String id = input.ascStr("Enter Item id: ");
+        Item item = tracker.findById(id);
+        String result = item == null ? "The Item has not contained in Array. Check ID for the Item." : item.getName();
+        System.out.println(result);
+    }
+    public static void findItemName(Input input, Tracker tracker) {
+        System.out.println("==== Find items by name ====");
+        String name = input.ascStr("Enter Item name: ");
+        Item[] items = tracker.findByName(name);
+        for (int i = 0; i < items.length; ++i) {
+            System.out.println("Name: " + items[i].getName() + ", ID: "
+                    + items[i].getId());
+        }
+    }
     private void showMenu() {
         System.out.println("<< Menu >>" + System.lineSeparator()
                 + "0. Add new Item" + System.lineSeparator()
@@ -20,43 +65,17 @@ public class StartUI {
             this.showMenu();
             int select = input.ascInt("Inter the menu number: ");
             if (select == 0) {
-                String name = input.ascStr("==== Create a new Item ===="
-                + System.lineSeparator() + "Enter name: ");
-                Item item = new Item(name);
-                tracker.add(item);
+                StartUI.createItem(input, tracker);
             } else if (select == 1) {
-                System.out.println("==== Tracker has contained ====");
-                Item[] items = tracker.findAll();
-                for (int i = 0; i < items.length; ++i) {
-                    System.out.println("Name: " + items[i].getName()
-                    + ", ID: " + items[i].getId());
-                }
+                StartUI.allItems(input, tracker);
             } else if (select == 2) {
-                String id = input.ascStr("==== Edit item ===="
-                + System.lineSeparator() + "Enter the item ID for replace: ");
-                String name = input.ascStr("Enter a new item name: ");
-                Item item = new Item(name);
-                String result = tracker.replace(id, item) ? "The operation was successful" : "The operation wasn't successful. Check ID for the item.";
-                System.out.println(result);
+                StartUI.editItem(input, tracker);
             } else if (select == 3) {
-                String id = input.ascStr("==== Delete item ===="
-                + System.lineSeparator() + "Enter Item id: ");
-                String result = tracker.delete(id) ? "The Item was deleted." : "The Item was not deleted. Check ID for the Item.";
-                System.out.println(result);
+                StartUI.deleteItem(input, tracker);
             } else if (select == 4) {
-                String id = input.ascStr("==== Find item by id ===="
-                + System.lineSeparator() + "Enter Item id: ");
-                Item item = tracker.findById(id);
-                String result = item == null ? "The Item has not contained in Array. Check ID for the Item." : item.getName();
-                System.out.println(result);
+                StartUI.findItemId(input, tracker);
             } else if (select == 5) {
-                String name = input.ascStr("==== Find items by name ===="
-                + System.lineSeparator() + "Enter Item name: ");
-                Item[] items = tracker.findByName(name);
-                for (int i = 0; i < items.length; ++i) {
-                    System.out.println("Name: " + items[i].getName() + ", ID: "
-                    + items[i].getId());
-                }
+                StartUI.findItemName(input, tracker);
             } else if (select == 6) {
                 System.out.println("Exit out of the program.");
                 run = false;
