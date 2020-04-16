c2c8a2f5a5d7992c513bd5d9a611042e41f1e72b
Alexander Nikolaev <nikolaevairman@gmail.com>
2020-04-16 21:36:27 +0300

    9.2. Тесты вывода на консоль в StartUI[#218954]

--- chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -1,11 +1,31 @@
 
 package ru.job4j.tracker;
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
+import java.util.StringJoiner;
 
 public class StartUITest {
+    // получаем ссылку на стандартный вывод в консоль.
+    PrintStream stdout = System.out;
+    // Создаем буфур для хранения вывода.
+    ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+    public void loadOutput() {
+        //Заменяем стандартный вывод на вывод в пямять для тестирования.
+        System.setOut(new PrintStream(out));
+    }
+
+    public void backOutput() {
+        // возвращаем обратно стандартный вывод в консоль.
+        System.setOut(stdout);
+    }
+
     @Test
     public void whenExit() {
         StubInput input = new StubInput(
@@ -15,6 +35,52 @@ public class StartUITest {
         new StartUI().init(input, new Tracker(), new UserAction[] {action});
         assertThat(action.isCall(), is(true));
     }
+    @Test
+    public void whenPrtMenu() {
+        loadOutput();
+        StubInput input = new StubInput(
+                new String[] {"0"}
+        );
+        StubAction action = new StubAction();
+        new StartUI().init(input, new Tracker(), new UserAction[] {action});
+        String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("Menu.")
+                .add("0. null")
+                .toString();
+        assertThat(new String(out.toByteArray()), is(expect));
+        backOutput();
+    }
+    @Test
+    public void whenFindAll() {
+        loadOutput();
+        Tracker tracker = new Tracker();
+        Item item = new Item("fix bug");
+        tracker.add(item);
+        ShowAllAction act = new ShowAllAction();
+        act.execute(new StubInput(new String[] {}), tracker);
+        String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("Name: " + item.getName() + ", ID: " + item.getId())
+                .toString();
+        assertThat(new String(out.toByteArray()), is(expect));
+        backOutput();
+    }
+    @Test
+    public void whenFindName() {
+        loadOutput();
+        Tracker tracker = new Tracker();
+        Item item = new Item("Test");
+        tracker.add(item);
+        FindNameAction act = new FindNameAction();
+        String itemName = item.getName();
+        String[] answers = {itemName};
+        act.execute(new StubInput(answers), tracker);
+        String expect = new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                .add("Name: " + item.getName() + ", ID: " + item.getId())
+                .toString();
+        assertThat(new String(out.toByteArray()), is(expect));
+        backOutput();
+    }
+
     /*
     @Test
     public void whenAddItem() {
