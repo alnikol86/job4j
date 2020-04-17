788b1c3299c37e6987789c2c950124b1d098d2fa
Alexander Nikolaev <nikolaevairman@gmail.com>
2020-04-17 21:10:00 +0300

    fix for 0. Видео[#218974]

--- chapter_002/src/main/java/ru/job4j/ex/BackArray.java
+++ chapter_002/src/main/java/ru/job4j/ex/BackArray.java
@@ -6,10 +6,10 @@ public class BackArray {
     public static void main(String[] args) {
         String[] names = {"Petr", "Ivan", "Nik", "Vasya"};
         int middle = names.length / 2;
-        for (int i = 0; i < middle; ++i) {
+        for (int i = 0; i <= middle - 1; ++i) {
             String temp = names[i];
-            names[i] = names[middle - i];
-            names[middle - i] = temp;
+            names[i] = names[(names.length - 1) - i];
+            names[(names.length - 1) - i] = temp;
         }
         System.out.println(Arrays.toString(names));
     }
