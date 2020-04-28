ba1e4cbd4dd878af07ffd2c2c1389684297bdd7b
Alexander Nikolaev <nikolaevairman@gmail.com>
2020-04-28 21:14:07 +0300

    update last commit

--- chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
+++ chapter_003/src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -12,13 +12,10 @@ public class PhoneDictionary {
     public ArrayList<Person> find(String key) {
         ArrayList<Person> result = new ArrayList<>();
         for (int i = 0; i < persons.size(); ++i) {
-            if (persons.get(i).getName().contains(key)) {
-                result.add(persons.get(i));
-            } else if (persons.get(i).getSurname().contains(key)) {
-                result.add(persons.get(i));
-            } else if (persons.get(i).getPhone().contains(key)) {
-                result.add(persons.get(i));
-            } else if (persons.get(i).getAddress().contains(key)) {
+            if (persons.get(i).getName().contains(key)
+                    || persons.get(i).getSurname().contains(key)
+                    || persons.get(i).getPhone().contains(key)
+                    || persons.get(i).getAddress().contains(key)) {
                 result.add(persons.get(i));
             }
         }
