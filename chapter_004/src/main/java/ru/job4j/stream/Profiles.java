fbf5fcdd40de701ac7b786c51f091ba74c66ab6a
Alexander Nikolaev <nikolaevairman@gmail.com>
2020-06-30 11:42:18 +0300

    2. Список адресов[110062#218799]refact

--- chapter_004/src/main/java/ru/job4j/stream/Profiles.java
+++ chapter_004/src/main/java/ru/job4j/stream/Profiles.java
@@ -1,19 +1,17 @@
 package ru.job4j.stream;
 
 import java.util.List;
-import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Profiles {
     List<Address> collect(List<Profile> profiles) {
         List<Address> customerAdresses = profiles.stream()
-                .map(new Function<Profile, Address>() {
-                    @Override
-                    public Address apply(Profile profile) {
-                        return profile.getAddress();
-                    }
-                })
+                .map(Profile::getAddress)
                 .collect(Collectors.toList());
         return customerAdresses;
+    }
+
+    private static Address getAdr(Profile profile) {
+        return profile.getAddress();
     }
 }
