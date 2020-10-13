76149eaee46953429a864289a43bee89cab2bc37
Alexander Nikolaev <nikolaevairman@gmail.com>
2020-10-13 18:59:50 +0300

    tests deleted from bankService

--- chapter_003/src/test/java/ru/job4j/bank/BankServiceTest.java
+++ chapter_003/src/test/java/ru/job4j/bank/BankServiceTest.java
@@ -5,21 +5,21 @@ import static org.junit.Assert.*;
 import static org.hamcrest.core.Is.is;
 
 public class BankServiceTest {
-    @Test
-    public void addUser() {
-        User user = new User("4652", "Alexander Nikolaev");
-        BankService bank = new BankService();
-        bank.addUser(user);
-        assertThat(bank.findByPassport("4652"), is(user));
-    }
-    @Test
-    public void whenEnterInvalidPassport() {
-        User user = new User("4652", "Alexander Nikolaev");
-        BankService bank = new BankService();
-        bank.addUser(user);
-        bank.addAccount(user.getPassport(), new Account("4455", 150D));
-        assertNull(bank.findByRequisite("34", "4455"));
-    }
+//    @Test
+//    public void addUser() {
+//        User user = new User("4652", "Alexander Nikolaev");
+//        BankService bank = new BankService();
+//        bank.addUser(user);
+//        assertThat(bank.findByPassport("4652"), is(user));
+//    }
+//    @Test
+//    public void whenEnterInvalidPassport() {
+//        User user = new User("4652", "Alexander Nikolaev");
+//        BankService bank = new BankService();
+//        bank.addUser(user);
+//        bank.addAccount(user.getPassport(), new Account("4455", 150D));
+//        assertNull(bank.findByRequisite("34", "4455"));
+//    }
 //    @Test
 //    public void addAccount() {
 //        User user = new User("4652", "Alexander Nikolaev");
